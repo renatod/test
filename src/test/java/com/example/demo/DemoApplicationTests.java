c841902ae9f977f65fbcd93b33a636b54684d93b
renatod <renato.dias@me.com.br>
2019-10-22 22:42:54 -0300

    que coisa hein

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.joda.time.DateTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+// -------------------------- OTHER METHODS --------------------------
+
+    @Test
+    void contextLoads() {
+        DateTime dateTime = new DateTime();
+    }
+}
